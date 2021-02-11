967646d0215f5d934f94ce0e58051dfc60f622c4
Tarek <tariqbeb@gmail.com>
2021-02-11 02:30:19 -0600

    introduced comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   
   root "posts#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210211060313) do
+ActiveRecord::Schema.define(version: 20210211075533) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.text     "comment",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.integer  "post_id",    limit: 4
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title",      limit: 255
